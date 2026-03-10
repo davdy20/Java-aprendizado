118fc3fea85e221da2fa49cc25de79870e8bf775
davdy01 <davdy2001@uni9.edu.br>
2026-03-10 14:06:01 -0300

    exercicio 1 completo

--- src/aula1/ex1.java
+++ src/aula1/ex1.java
@@ -1,5 +1,31 @@
 package aula1;
 
-public class ex1 {
+import java.util.Locale;
+import java.util.Scanner;
 
+// um programa para ler dois valores inteiros,
+//e depois mostrar na tela a soma desses números com uma mensagem explicativa.
+public class ex1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		//Declaração de variaveis
+		int n1, n2, soma;
+		
+		//Entrada de dados
+		System.out.println("Digite o Primeiro número: ");
+		n1 = sc.nextInt();
+		
+		System.out.println("Digite o segundo número: ");
+		n2 = sc.nextInt();
+		
+		//Processamento de dados
+		soma = n1 + n2;
+		
+		//Saída de dados
+		System.out.printf("A soma entre %d e %d é: %d",n1, n2, soma);
+		
+		sc.close();
+	}
 }
