ae594ff5b0cfb981fac565c857adb5f67154fb1f
davdy01 <davdy2001@uni9.edu.br>
2026-03-14 05:38:18 -0300

    exercicio seis concluido

--- src/aula1/ex6.java
+++ src/aula1/ex6.java
@@ -9,8 +9,10 @@ public class ex6 {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		//Declaração de variavel
 		double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
 		
+		//Entrada de dados
 		System.out.println("Digite o valor de A: ");
 		A = sc.nextDouble();
 		
@@ -20,12 +22,14 @@ public class ex6 {
 		System.out.println("Digite o valor de C: ");
 		C = sc.nextDouble();
 		
+		//Processamento de dados
 		triangulo = A * C / 2.0;
 		circulo = 3.14159 * C * C;
 		trapezio = (A + B) / 2.0 * C;
 		quadrado = B * B;
 		retangulo = A * B;
 		
+		//Saída de dados
 		System.out.printf("O Valor do triangulo é: %.3f\n ", triangulo);
 		System.out.printf("O Valor do circulo é: %.3f\n ", circulo);
 		System.out.printf("O Valor do trapézio é: %.3f\n ", trapezio);
