eb062f976c182bbd10218092384ffe09cdb97249
davdy01 <davdy2001@uni9.edu.br>
2026-03-14 05:40:26 -0300

    exercicio quatro concluido

--- src/aula1/ex4.java
+++ src/aula1/ex4.java
@@ -8,9 +8,12 @@ public class ex4 {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		//Declaração de varíaveis
 		int numero, horas;
 		double salario, valorHora;
 		
+		//Entrada de dados
 		System.out.println("Digite o número do funcionario: ");
 		numero = sc.nextInt();
 		
@@ -20,8 +23,10 @@ public class ex4 {
 		System.out.println("Digite o valor da hora trabalhada: ");
 		valorHora = sc.nextDouble();
 		
+		//Processamento de dados
 		salario = valorHora * horas;
 		
+		//Saída de dados 
 		System.out.println("Numero = " + numero);
 		System.out.printf("Seu Salario é = $ %.2f\n", salario);
 		
