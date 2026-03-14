0fb2a9f772e90a5cb60902e556c862072497292e
davdy01 <davdy2001@uni9.edu.br>
2026-03-14 05:36:18 -0300

    exercicio sete concluido

--- src/aula1/ex7.java
+++ src/aula1/ex7.java
@@ -6,11 +6,14 @@ public class ex7 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		//Declaração de variavel
 		int n;
 		
+		//Entrada de dados
 		System.out.println("Digite um número: ");
 		n = sc.nextInt();
 		
+		//Estrutra de decisão e saída de dados
 		if (n >= 0) {
 			System.out.println("Positivo");
 		}
