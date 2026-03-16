1954fc12f1b3e4572e66111e0ccac26e00502af7
davdy01 <davdy2001@uni9.edu.br>
2026-03-16 20:08:04 -0300

    Exercicio doze concluido

---
+++ src/aula1/ex12.java
@@ -0,0 +1,40 @@
+package aula1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex12 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//Declaração de varíaveis
+		double numero;
+		
+		//Entrada de dados
+		System.out.println("Digite um numero: ");
+		numero = sc.nextDouble();
+		
+		//Estrtura de decisão
+		//Processamento de dados
+		if (numero < 0.0 || numero > 100.0) {
+			System.out.println("Fora do intervalo");
+		}
+		else if (numero <= 25.0) {
+			System.out.println("Intervalo entre [0,25]");
+		}
+		else if (numero <= 50.0) {
+			System.out.println("Intervalo entre [25,50]");
+		}
+		else if (numero <= 75.0){
+			System.out.println("Intervalo entre [50,75]");
+		}		
+		else {
+			System.out.println("Intervalo entre [75,100]");
+		}		
+		
+		sc.close();
+		
+}
+}
