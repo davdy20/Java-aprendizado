601e1faaf2a1f529935d3edd7d7afaa22a706010
davdy20 <davdy2001@gmail.com>
2026-03-16 20:30:39 -0300

    Exercicio doze concluido

--- src/aula1/ex12.java
+++ src/aula1/ex12.java
@@ -23,16 +23,16 @@ public class ex12 {
 			System.out.println("Fora do intervalo");
 		}
 		else if (numero <= 25.0) {
-			System.out.println("Intervalo entre [0,25]");
+			System.out.println("Intervalo [0,25]");
 		}
 		else if (numero <= 50.0) {
-			System.out.println("Intervalo entre [25,50]");
+			System.out.println("Intervalo [25,50]");
 		}
 		else if (numero <= 75.0){
-			System.out.println("Intervalo entre [50,75]");
+			System.out.println("Intervalo [50,75]");
 		}		
 		else {
-			System.out.println("Intervalo entre [75,100]");
+			System.out.println("Intervalo [75,100]");
 		}		
 		
 		sc.close();
